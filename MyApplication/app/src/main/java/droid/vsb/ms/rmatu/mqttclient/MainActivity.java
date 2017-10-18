1d09afeb70e443da3c7e769c27b4dfe36a137f02
Rene Matuszek <rene.matuszek@navmatix.com>
2017-10-18 22:36:52 +0200

    init project

---
+++ MyApplication/app/src/main/java/droid/vsb/ms/rmatu/mqttclient/MainActivity.java
@@ -0,0 +1,13 @@
+package droid.vsb.ms.rmatu.mqttclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
