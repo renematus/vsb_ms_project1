bde1b9cc013480aeea8115452e9067a051a70794
Rene Matuszek <rene.matuszek@navmatix.com>
2017-10-20 21:07:20 +0200

    add subscribe

---
+++ MyApplication/app/src/main/java/droid/vsb/ms/rmatu/mqttclient/Constant.java
@@ -0,0 +1,13 @@
+package droid.vsb.ms.rmatu.mqttclient;
+
+/**
+ * Created by renematuszek on 20/10/2017.
+ */
+
+public class Constant {
+
+    public static final String serverHostName = "pcfeib425t.vsb.cz";
+    public static final int serverPort = 1883;
+    public static final String clientId = "TestClient";
+
+}
--- MyApplication/app/src/main/java/droid/vsb/ms/rmatu/mqttclient/MainActivity.java
+++ MyApplication/app/src/main/java/droid/vsb/ms/rmatu/mqttclient/MainActivity.java
@@ -3,7 +3,14 @@ package droid.vsb.ms.rmatu.mqttclient;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import droid.vsb.ms.rmatu.mqttclient.Business.Connection;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Connection connection;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
